d602228467c3c10d1a10219e5535ea6609041b82
Josep Subils <josep@spellbook.io>
2017-10-30 12:02:52 +0100

    [release] 0.0.1

--- example/booleantest.js
+++ example/booleantest.js
@@ -1,16 +1,16 @@
 const bool = require('../src/searchbool.js')
 
-const boolarr = bool('hola AND adios OR debian AND redhat OR pepe AND holas AND bu')
+const boolarr = bool('hello AND bye OR debian AND redhat OR josep AND hello AND foo')
 console.log(JSON.stringify(boolarr))
 
-const boolarr1 = bool('hola AND adios')
+const boolarr1 = bool('hello AND bye')
 console.log(JSON.stringify(boolarr1))
 
-const boolarr2 = bool('hola OR adios')
+const boolarr2 = bool('hello OR bye')
 console.log(JSON.stringify(boolarr2))
 
-const boolarr3 = bool('hola AND adios AND hi AND buuu')
+const boolarr3 = bool('hello AND bye AND hi AND foo')
 console.log(JSON.stringify(boolarr3))
 
-const boolarr4 = bool('hola')
+const boolarr4 = bool('hello')
 console.log(JSON.stringify(boolarr4))
